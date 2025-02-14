03b349104c8abec96d89bfacb1f370221cc8340e
Sakib Hossen <im.sakibhossen@gmail.com>
2025-02-14 16:53:20 +0600

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>AI Avatar Generator</h1>
+      <p>Create unique avatars using text prompts</p>
+    </header>
+  );
+}
+
+export default Header;
